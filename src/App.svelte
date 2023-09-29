8f8a935341bbe663b82e98e0f7e154a83227880c
jindorry <ekfaktldk11@gmail.com>
2023-09-29 21:44:22 +0900

    add Bulletin component

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,45 @@
 <script>
   import Card from './client/Card.svelte';
+  import Bulletin from './client/Bulletin.svelte';
 </script>
 
-<main>
-  <div class="cardContainer">
-    <Card titleName="Home" />
-    <Card titleName="MyProfile" />
-    <Card titleName="LeaderBoard" />
+<main class="main">
+  <div class="nav">
+    <div class="cardContainer">
+      <Card titleName="Home" />
+      <Card titleName="MyProfile" />
+      <Card titleName="LeaderBoard" />
+    </div>
+  </div>
+  <div class="bulletin">
+    <Bulletin num="1" />
+    <Bulletin num="2" />
+    <Bulletin num="3" />
+    <Bulletin num="4" />
+    <Bulletin num="5" />
   </div>
 </main>
 
 <style>
+  /* navigaion bar 구현해야함 */
+  .main {
+    position: absolute;
+    top: 5em;
+    left: 2em;
+  }
+
   .cardContainer {
     display: flex;
     flex-direction: row;
     position: absolute;
     top: 1em;
     right: 1em;
+    position: fixed;
+    background-color: white;
+  }
+
+  .bulletin {
+    display: flex;
+    flex-direction: column;
   }
 </style>
---
+++ src/client/Bulletin.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import Title from "./Title.svelte";
+
+  export let num = "";
+</script>
+
+<div class="main-container">
+  <p class="bulletin-num">#{parseInt(num)}</p>
+  <div class="title-container">
+    <h2>Given words</h2>
+  </div>
+</div>
+
+<style>
+  .main-container {
+    display: inherit;
+    flex-direction: row;
+    margin: 0.4em;
+    padding: 5px 10px;
+    border: 0.1em solid magenta;
+    border-radius: 5px;
+    width: 20em;
+    height: 10em;
+  }
+
+  .main-container:hover {
+    background-color: #E0E2E1;
+  }
+
+  .bulletin-num {
+    margin: 0;
+    align-items: flex-start;
+  }
+
+  h2 {
+    position: relative;
+    top: 0;
+    font-size: 1.5em;
+  }
+</style>
