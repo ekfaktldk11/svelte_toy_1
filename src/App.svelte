62438bb42e45552a77066db460b765bfa50a6f2d
jindorry <ekfaktldk11@gmail.com>
2023-10-02 17:32:24 +0900

    add NavBar

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,11 @@
 <script>
-  import Card from './client/Card.svelte';
+  import NavBar from './client/NavBar.svelte';
   import Bulletin from './client/Bulletin.svelte';
 </script>
 
 <main class="main">
   <div class="nav">
-    <div class="cardContainer">
-      <Card titleName="Home" />
-      <Card titleName="MyProfile" />
-      <Card titleName="LeaderBoard" />
-    </div>
+    <NavBar />
   </div>
   <div class="bulletin">
     <Bulletin num="1" />
@@ -17,29 +13,41 @@
     <Bulletin num="3" />
     <Bulletin num="4" />
     <Bulletin num="5" />
+    <Bulletin num="6" />
+    <Bulletin num="7" />
+    <Bulletin num="8" />
+    <Bulletin num="9" />
+    <Bulletin num="10" />
+    <Bulletin num="11" />
+    <Bulletin num="12" />
+    <Bulletin num="13" />
   </div>
 </main>
 
 <style>
-  /* navigaion bar 구현해야함 */
   .main {
-    position: absolute;
-    top: 5em;
-    left: 2em;
+    z-index: 1;
   }
 
-  .cardContainer {
+  .nav {
     display: flex;
-    flex-direction: row;
-    position: absolute;
-    top: 1em;
-    right: 1em;
     position: fixed;
+    width: 100%;
+    height: 5rem;
+    z-index: 3;
     background-color: white;
+    border-bottom: 0.1rem #ABB8C3 solid;
   }
 
   .bulletin {
-    display: flex;
-    flex-direction: column;
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    grid-template-columns: repeat(3, 1fr);
+    margin-top: 0.5rem;
+    position: absolute;
+    top: 5rem;
+    left: 0;
+    width: 100%;
   }
 </style>
---
+++ src/client/NavBar.svelte
@@ -0,0 +1,21 @@
+<script>
+  import NavButton from "./NavButton.svelte";
+</script>
+
+<div class="btn-container">
+  <NavButton titleName="Home" />
+  <NavButton titleName="MyProfile" />
+  <NavButton titleName="LeaderBoard" />
+</div>
+
+<style>
+  .btn-container {
+    display: flex;
+    flex-direction: row;
+    position: absolute;
+    top: 1em;
+    right: 1em;
+    position: fixed;
+    background-color: white;
+  }
+</style>
